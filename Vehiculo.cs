b7c7d32e2d80594f9b4be4473653eacca90b4ebe
Bladimir Galvez <bladimirgalvez11@mesoamericana.edu.gt>
2022-03-11 09:18:24 -0600

    Agregar archivos de proyecto.

---
+++ Vehiculo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repaso2D
+{
+    class Vehiculo
+    {
+        public string placa { get; set; }
+        public string marca { get; set; }
+        public string color { get; set; }
+        public string modelo { get; set; }
+
+        public decimal precioKilometro { get; set; }
+
+    }
+}
